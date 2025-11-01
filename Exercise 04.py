a7b9d0a205987ab35e22b989a5a449830f40e430
tyyyre <saga08190@gmail.com>
2025-11-01 21:30:13 +0400

    Exercise 04.py

---
+++ Exercise 04.py
@@ -0,0 +1,14 @@
+question = input("What is the capital of France?");
+correctanswer = "Paris";
+
+if question == correctanswer:
+    print("Answer is correct!");
+else:
+    print("Answer is wrong!");
+
+# if the answer is correct, print "Answer is correct!", otherwise print "Answer is wrong!"
+# Example:
+# Input: Paris
+# Output: Answer is correct!
+# Input: London
+# Output: Answer is wrong!
