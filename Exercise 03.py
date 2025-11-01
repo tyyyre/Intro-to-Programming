b1baa74977952526d6246ae75386cb8a17ede831
tyyyre <saga08190@gmail.com>
2025-11-01 21:39:14 +0400

    Advance Requirements

--- Exercise 03.py
+++ Exercise 03.py
@@ -1,11 +1,6 @@
-name = "Hamza";
-hometown = "Karachi";
-age = "20";
+name = input("Enter your name: ");
+hometown = input("Enter your hometown: ");
+age = input("Enter your age: ");
 
 # Print the values on separate lines using a single print() statement.
 print(name + "\n" + hometown + "\n" + age)
-
-# Output:
-# Hamza
-# Karachi
-# 20
