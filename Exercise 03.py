03568f8ddb9b154602db3d25b99e59ead5157d5f
tyyyre <saga08190@gmail.com>
2025-11-01 21:22:14 +0400

    Exercise 03.py
    Prints name, hometown, and age on separate lines.

---
+++ Exercise 03.py
@@ -0,0 +1,11 @@
+name = "Hamza";
+hometown = "Karachi";
+age = "20";
+
+# Print the values on separate lines using a single print() statement.
+print(name + "\n" + hometown + "\n" + age)
+
+# Output:
+# Hamza
+# Karachi
+# 20
