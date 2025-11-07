96fa14faf8f108a3e98a0d6f5af75e31ee9e4e65
tyyyre <saga08190@gmail.com>
2025-11-07 18:53:22 +0400

    Advance Requirements

--- Exercise 06.py
+++ Exercise 06.py
@@ -1,9 +1,14 @@
 correctpassword = 12345
+allowedattempts = 5
 
 while True:
     password = int(input("Enter your password: "))
+    allowedattempts -= 1
+    if allowedattempts == 0:
+        print("No more attempts left. Access denied!")
+        break
     if password == correctpassword:
         print("Access granted!")
         break
     else:
-        print("Access denied! Try again.")
+        print("Access denied! Try again. Attempts left:", allowedattempts)
