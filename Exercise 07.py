264558fa986a856d1038b75ad53c00981f1fff4a
tyyyre <saga08190@gmail.com>
2025-11-07 19:10:30 +0400

    Exercise 07

---
+++ Exercise 07.py
@@ -0,0 +1,18 @@
+def function(start, end, increment):
+    for i in range(start, end, increment):
+        print(i)
+# Counting from 0 to 50 in increment of 1
+print("Counting from 0 to 50 in increment of 1")
+function(0, 51, 1)
+# Counting from 50 to 0 in decrement of 1
+print("Counting from 50 to 0 in decrement of 1")
+function(50, -1, -1)
+# Counting from 30 to 50 in increment of 1
+print("Counting from 30 to 50 in increment of 1")
+function(30, 51, 1)
+# Counting from 50 to 10 in increment of 2
+print("Counting from 50 to 10 in decrement of 2")
+function(50, 9, -2)
+# Counting from 100 to 200 in increment of 5
+print("Counting from 100 to 200 in increment of 5")
+function(100, 201, 5)
