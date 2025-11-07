d54ecb5b142847c46a6dd268e873d3a397cca176
tyyyre <saga08190@gmail.com>
2025-11-07 18:43:34 +0400

    Exercise 05

---
+++ Exercise 05.py
@@ -0,0 +1,22 @@
+# dictionary of months and their corresponding number of days
+table = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+question = "Which month's number of days do you want to know?"
+answer = int(input(question + " "))
+if answer in table:
+    print(f"Month {answer} has {table[answer]} days.")
+else:
+    print("Invalid month number!")
