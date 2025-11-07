2e2b45633d6b9a2964f0c168d3c3e786e0e4dfa2
tyyyre <saga08190@gmail.com>
2025-11-07 19:18:13 +0400

    Exercise 08

---
+++ Exercise 08.py
@@ -0,0 +1,7 @@
+lookingfor = input("What user are you looking for? ")
+users = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+if lookingfor in users:
+    print(f"User {lookingfor} found!")
+else:
+    print(f"User {lookingfor} not found!")
