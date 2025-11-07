859463c261fb9d0b82af7d78378687092b79bfc7
tyyyre <saga08190@gmail.com>
2025-11-07 18:15:58 +0400

    Advance Requirements

--- Exercise 04.py
+++ Exercise 04.py
@@ -1,14 +1,15 @@
-question = input("What is the capital of France?");
-correctanswer = "Paris";
+question = "What is the capital of France?"
+answer = input(question + " ")
+correctanswer = "paris";
 
-if question == correctanswer:
+if answer.lower() == correctanswer:
     print("Answer is correct!");
 else:
     print("Answer is wrong!");
 
 # if the answer is correct, print "Answer is correct!", otherwise print "Answer is wrong!"
 # Example:
-# Input: Paris
+# Input: PaRiS (regardless of capital letters)
 # Output: Answer is correct!
 # Input: London
 # Output: Answer is wrong!
