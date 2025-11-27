1796490424a08329bf2152b6ac7db2e6dc5870df
tyyyre <saga08190@gmail.com>
2025-11-27 20:18:06 +0400

    Added Exercise 8 Simple Search solution

--- Exercise-8/Exercise 08.py
+++ Exercise-8/Exercise 08.py
@@ -1,5 +1,9 @@
+# Exercise 8: Simple Search
+# This program searches for a user-input name within a predefined list of names.
+
 lookingfor = input("What user are you looking for? ")
-users = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+users = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
 if lookingfor in users:
     print(f"User {lookingfor} found!")
