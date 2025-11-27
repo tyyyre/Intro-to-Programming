9e6886df4b67a555d097ed6aaa7e810c662f332e
tyyyre <saga08190@gmail.com>
2025-11-27 19:57:08 +0400

    Make a program to print 'Coding is Cool'
    This program prints the phrase 'Coding is Cool' using variables and string concatenation.

---
+++ Exercise-1/Exercise 1.py
@@ -0,0 +1,9 @@
+# Exercise 1: Coding is Cool
+# This program prints the phrase "Coding is Cool" using variables and string concatenation.
+
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# Print the concatenated phrase
+print(word1, word2, word3)
