cc2c21f7c4d4febdad535d0944d2d9bdd75f0680
tyyyre <saga08190@gmail.com>
2025-11-27 20:21:39 +0400

    Added Exercise 9 – Hello solution

--- Exercise 09.py
+++
@@ -1,8 +0,0 @@
-def hello():
-    print("Hello") # Fill in this blank to print "Hello" to the console
-
-def main():
-    hello()  # Fill in this blank to call the hello() function
-
-if __name__ == "__main__":
-    main()
---
+++ Exercise-9/Exercise 09.py
@@ -0,0 +1,12 @@
+# Exercise 9: Hello
+# This program defines a function hello() that prints "Hello"
+# and calls it from the main function.
+
+def hello():
+    print("Hello")  # Prints "Hello" to the console
+
+def main():
+    hello()  # Call the hello() function
+
+if __name__ == "__main__":
+    main()
